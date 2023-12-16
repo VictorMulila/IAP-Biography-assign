e93d2dda633b2d7f5fa82e2c60994b7cf18d1032
VictorMulila <146417375+VictorMulila@users.noreply.github.com>
2023-12-16 03:51:33 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function submitForm() {
+    // Here you can add JavaScript logic to handle form submission
+    // For simplicity, let's just display a message
+    var resultDiv = document.getElementById("result");
+    resultDiv.innerHTML = "Form submitted successfully!";
+}
